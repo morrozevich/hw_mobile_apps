3b5de28ae31c76699a06432a3e5621f078424529
morrozevich <150383856+morrozevich@users.noreply.github.com>
2024-11-05 15:07:57 +0100

    names

--- app/src/main/java/com/example/myapplication/registerWindow.kt
+++ app/src/main/java/com/example/myapplication/registerWindow.kt
@@ -3,7 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class registerWindow : AppCompatActivity() {
+class RegisterWindow : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_window)
